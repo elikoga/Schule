4eebfb718ac3181629063aabee8ead6783979b7a
Eli Kogan-Wang <elikowa@gmail.com>
2019-12-11 14:11:22 +0100

    Info Stunde

---
+++ Informatik/Projects/Buecherregal_Impl/src/buecherregal/List.java
@@ -0,0 +1,130 @@
+package buecherregal;
+
+public class List<ContentType> {
+
+    public class ListNode {
+
+        private ContentType contentObject;
+        private ListNode next;
+
+        public ListNode(ContentType pContent) {
+            contentObject = pContent;
+            next = null;
+        }
+
+        public ContentType getContentObject() {
+            return contentObject;
+        }
+
+        public void setContentObject(ContentType pContent) {
+            contentObject = pContent;
+        }
+
+        public ListNode getNextNode() {
+            return this.next;
+        }
+
+        public void setNextNode(ListNode pNext) {
+            this.next = pNext;
+        }
+
+    }
+    ListNode first;
+
+    ListNode last;
+
+    ListNode current;
+
+    public List() {
+        first = null;
+        last = null;
+        current = null;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public boolean hasAccess() {
+        return current != null;
+    }
+
+    public void next() {
+        current = current.next;
+    }
+
+    public void toFirst() {
+        current = first;
+    }
+
+    public void toLast() {
+        current = last;
+    }
+
+    public ContentType getContent() {
+        if (!hasAccess()) {
+            return null;
+        }
+        return current.getContentObject();
+    }
+
+    public void setContent(ContentType pContent) {
+        if (!hasAccess()) {
+            return;
+        }
+        current.setContentObject(pContent);
+    }
+
+    public void insert(ContentType pContent) {
+
+    }
+
+    public void append(ContentType pContent) {
+        ListNode newL = new ListNode(pContent);
+        if (isEmpty()) {
+            first = newL;
+            current = newL;
+            last = newL;
+        } else {
+            ListNode after = current.getNextNode();
+            current.setNextNode(newL);
+            newL.setNextNode(after);
+        }
+
+    }
+
+    public void concat(List<ContentType> pList) {
+        if (pList != null && !pList.isEmpty()) { // Nichts tun, wenn pList leer oder nicht existent.
+
+            if (this.isEmpty()) { // Fall: An leere Liste anfuegen.
+                this.first = pList.first;
+                this.last = pList.last;
+            } else { // Fall: An nicht-leere Liste anfuegen.
+                this.last.setNextNode(pList.first);
+                this.last = pList.last;
+            }
+
+            // Liste pList loeschen.
+            pList.first = null;
+            pList.last = null;
+            pList.current = null;
+        }
+    }
+
+    public void remove() {
+
+    }
+
+    private ListNode getPrevious(ListNode pNode) {
+        if (pNode != null && pNode != first && !this.isEmpty()) {
+            ListNode temp = first;
+            while (temp != null && temp.getNextNode() != pNode) {
+                temp = temp.getNextNode();
+            }
+            return temp;
+        } else {
+            return null;
+        }
+    }
+
+}
