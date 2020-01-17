1dd5b53bfdd9897ddb3e926aee3c6c8cb6b434aa
Eli Kogan-Wang <elikowa@gmail.com>
2020-01-17 07:54:58 +0100

    Informatik Stunde

---
+++ Informatik/Projects/Ahnenbaum/src/ahnenbaum/Ahne.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahnenbaum;
+
+/**
+ *
+ * @author Harri
+ */
+public class Ahne {
+    String name;
+    
+    public Ahne(String pName)
+    {
+        this.name = pName;
+    }
+    
+    public String gibName()
+    {
+        return this.name;
+    }
+}
---
+++ Informatik/Projects/Ahnenbaum/src/ahnenbaum/Ahnenbaum.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahnenbaum;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Harri
+ */
+public class Ahnenbaum {
+
+ private BinaryTree<Ahne> baum;
+ private String eingabe;
+ 
+ 
+ public Ahnenbaum()
+ {
+     
+     ;
+     
+ }
+ 
+ public BinaryTree erstellung (String eingabe)
+ {
+    ;
+    
+ }
+ 
+ public String IO(BinaryTree<Ahne> baum, String platzhalter)
+{
+    ;
+}
+ 
+ public String toString()
+ {
+     return ;
+ }
+ 
+ public int Niveau()
+ {
+     return ;
+ }
+ 
+ public int gibNiveau (BinaryTree baum)
+ {
+    ;
+ }
+    
+}
