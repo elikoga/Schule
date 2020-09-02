b1e6da9595bb7d7d59ba546141b5a9d22555c030
Eli Kogan-Wang <elikowa@gmail.com>
2020-09-02 11:14:08 +0200

    Informatik Stunde

---
+++ Informatik/Projects/Gleitkommaautomat/src/gleitkommaautomat/Akzeptor.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gleitkommaautomat;
+
+/**
+ *
+ * @author Harri
+ */
+public abstract class Akzeptor {
+    
+    private int zustand;
+    
+    public Akzeptor()
+    {
+        zustand=0;
+    }
+    
+    public int gibZustand()
+    {
+        return zustand;
+    }
+    
+    public void setzeInAnfangszustand()
+    {
+        zustand=0;
+    }
+    
+    public void setzeZustand (int pZustand)
+    {
+        zustand=pZustand;
+    }
+    
+    public boolean teste (String pEingabeString)
+    {
+        int index;
+        char zeichen;
+        
+        setzeInAnfangszustand();
+        index=1;
+        while (index<=pEingabeString.length() && (zustand!=-1))
+        {
+            zeichen=pEingabeString.charAt(index);
+            if (!imAlphabet(zeichen))
+            {
+                zustand=-1;
+            }
+            else
+            {
+                zustandsuebergang(zeichen);
+            }
+            index=index+1;
+        }
+        return imEndzustand();
+    }
+    
+    public abstract void zustandsuebergang( char pEingabe);
+    
+    public abstract boolean imEndzustand();
+    
+    public abstract boolean imAlphabet(char pEingabe);
+}
---
+++ Informatik/Projects/Gleitkommaautomat/src/gleitkommaautomat/Gleitkommaautomat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gleitkommaautomat;
+
+/**
+ *
+ * @author Harri
+ */
+public class Gleitkommaautomat extends Akzeptor {
+
+    public void zustandsuebergang( char pEingabe)
+    {
+        ;
+    }
+    
+    public boolean imEndzustand()
+    {
+        return false;
+    }
+    
+    public boolean imAlphabet(char pEingabe)
+   {
+       return false;
+   }
+    
+}
